391ac16ff2ad801498537ce4ad7bf50729b8c84f
liyiqi <yli12354@terpmail.umd.edu>
2026-01-20 20:47:12 +0800

    HW-07 Implement Matmul inferShape

--- src/operators/matmul.cc
+++ src/operators/matmul.cc
@@ -27,7 +27,42 @@ namespace infini
         // TODO：返回经过 matmul 操作后的 shape
         // REF: https://github.com/onnx/onnx/blob/main/docs/Operators.md#gemm
         // =================================== 作业 ===================================
-        return std::nullopt;
+        if (inputs.size() != 2) {
+            IT_ASSERT(inputs.size() == 2, "Matmul requires two inputs.");
+        }
+        
+        const auto A = inputs[0], B = inputs[1];
+        auto shapeA = A->getDims();
+        auto shapeB = B->getDims();
+
+        // A[m, k] * B[k, n] = C[m, n]
+        int m, n, k;
+        if (transA) {
+            m = shapeA[shapeA.size() - 1];
+            k = shapeA[shapeA.size() - 2];
+        } else {
+            m = shapeA[shapeA.size() - 2];
+            k = shapeA[shapeA.size() - 1];
+        }
+
+        if (transB) {
+            n = shapeB[shapeB.size() - 2];
+            IT_ASSERT(k == shapeB[shapeB.size() - 1]);
+        } else {
+            n = shapeB[shapeB.size() - 1];
+            IT_ASSERT(k == shapeB[shapeB.size() - 2]);
+        }
+
+        Shape outputShape;
+
+        if (shapeA.size() > 2) {
+            outputShape.insert(outputShape.end(), shapeA.begin(), shapeA.end() - 2);
+        }
+
+        outputShape.push_back(m);
+        outputShape.push_back(n);
+
+        return {{outputShape}};
     }
 
 } // namespace infini
